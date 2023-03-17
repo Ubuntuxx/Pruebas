14870e316cf1e9facbbfd69fe0fcd8c30f85ae38
Jose Luis <unixluisx@protonmail.ch>
2023-03-17 16:42:45 -0600

    Base de datos

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
+import java.sql.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        try {
+            Connection connection = DriverManager.getConnection("jdbc:sqlite:/tmp/java/untitled/testjava.db");
+            Statement statement = connection.createStatement();
+            statement.execute("CREATE TABLE contacts (name TEXT, phone INTEGER, email TEXT)");
+            statement.close();
+            connection.close();
+        } catch (SQLException e) {
+            System.out.println("Algo fue mal!: " + e.getMessage());
+        }
     }
 }
